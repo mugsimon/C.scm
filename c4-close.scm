d2686a5394363ab6f0d256b077bbb066dc7113cc
mugsimon <mug24375826dev@gmail.com>
2018-10-02 19:27:43 +0900

    add c4-close, for normal syntax scheme

---
+++ c4-close.scm
@@ -0,0 +1,178 @@
+;;; 自由変数のクロージングを行う
+;;; letrec式の束縛内になる自由変数を持つlambda式の引数に自由変数を追加する
+;;; 自由変数を追加したローカル関数呼び出しの引数に自由変数を追加する
+
+;; (define var (lambda requireds rest body))
+;; (define var expr)
+;; (begin ...)
+;; (fun ...)
+(define (c.scm:c4close x)
+     (if (pair? x)
+         (case (car x)
+           ((define)
+            (let ((form (caddr x)))
+              (if (and (pair? form)
+                       (eq? (car form) 'lambda))
+                  (c.scm:c4close-function (car x) ;; define
+                                            (cadr x) ;; name
+                                            (caddr x)) ;; (lambda params body)
+                  `(,(car x) ,(cadr x) ,(c.scm:c4close-expr form)))))
+           ((begin)
+            `(begin ,@(map c.scm:c4close (cdr x))))
+           (else
+            (c.scm:c4close-expr x)))
+         (c.scm:c4close-expr x)))
+
+(define (c.scm:c4close-function first name lambda-expr)
+  (let ((x (c4expr lambda-expr)))
+    `(,first ,name ,x)))
+
+;; form->expr
+(define (c.scm:c4close-expr form)
+  (let ((x (c4expr form)))
+    x))
+
+(define (c.scm:symbol? x)
+  (or (c.scm:var? x)
+      (symbol? x)))
+
+(define (c.scm:pair? x)
+  (and (not (c.scm:var? x))
+       (pair? x)))
+
+(define (c.scm:var? x)
+  (and (list? x)
+       (= (length x) 7)
+       (symbol? (var-name x))
+       (boolean? (var-funarg x))
+       (boolean? (var-assigned x))
+       (boolean? (var-closed x))
+       (or (boolean? (var-local-fun x))
+           (list? (var-local-fun x)))
+       (list? (var-local-fun-args x))
+       (list? (var-loc x))))
+
+(define-syntax dolist
+  (syntax-rules ()
+    ((dolist (var lst) body ...)
+     (let loop ((rest lst))
+       (cond ((null? rest)
+              '())
+             (else
+              (let ((var (car rest)))
+                (begin body ...)
+                (loop (cdr rest)))))))))
+
+(define (c.scm:union x y)
+  (cond ((null? x)
+         y)
+        ((member (car x) y)
+         (c.scm:union (cdr x) y))
+        (else
+         (cons (car x) (c.scm:union (cdr x) y)))))
+
+(define (c.scm:difference x y)
+  (cond ((null? x)
+         '())
+        ((member (car x) y)
+         (c.scm:difference (cdr x) y))
+        (else
+         (cons (car x) (c.scm:difference (cdr x) y)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (c4expr form)
+  (cond ((c.scm:pair? form)
+         (let ((fun (car form))
+               (args (cdr form)))
+           (cond ((c.scm:symbol? fun)
+                  (case fun
+                    ((if) (c4if args))
+                    ((and) (c4and args))
+                    ((or) (c4or args))
+                    ((begin) (c4begin args))
+                    ((lambda) (c4lambda args))
+                    ((delay) (c4delay args))
+                    ((let) (c4let args))
+                    ((let*) (c4let args))
+                    ((letrec) (c4letrec args))
+                    ((set!) (c4set! args))
+                    ((quote) (c4quote args))
+                    (else
+                     (c4symbol-fun fun args))))
+                 (else
+                  `(,(c4expr fun) ,@(c4args args))))))
+        (else
+         (case form
+           ((#f) #f)
+           ((#t) #t)
+           ((()) '())
+           (else
+            form)))))
+
+(define (c4if args)
+  `(if ,(c4expr (car args))
+       ,(c4expr (cadr args))
+       ,(c4expr (caddr args))))
+
+(define (c4and args)
+  `(and ,@(map c4expr args)))
+
+(define (c4or args)
+  `(or ,@(map c4expr args)))
+
+(define (c4begin args)
+  `(begin ,@(map c4expr args)))  
+
+(define (c4lambda args . free-vars)
+  (let ((params (car args))
+        (body (cadr args)))
+    (list 'lambda (c.scm:union (if (null? free-vars)
+                                   '()
+                                   (car free-vars))
+                               params) (c4expr body))))
+
+(define (c4let args)
+  (let loop ((defs (car args))
+             (cdefs '()))
+    (cond ((null? defs)
+           `(let ,(reverse cdefs) ,(c4expr (cadr args))))
+          (else
+           (loop (cdr defs)
+                 (cons (c4def (car defs))
+                       cdefs))))))
+
+(define (c4def def)
+  (let ((var (car def))
+        (form (cadr def)))
+    (list var (if (var-local-fun var)
+                  (c4lambda (cdr form) (var-local-fun var))
+                  (c4expr form)))))
+
+(define (c4letrec args)
+  (let loop ((defs (car args))
+             (cdefs '()))
+    (cond ((null? defs)
+           `(letrec ,(reverse cdefs) ,(c4expr (cadr args))))
+          (else
+           (loop (cdr defs)
+                 (cons (c4def (car defs))
+                       cdefs))))))
+
+(define (c4set! args)
+  `(set! ,(car args) ,(c4expr (cadr args))))
+
+(define (c4quote args)
+  `(quote ,(car args)))
+
+(define (c4symbol-fun fun args)
+  (if (and (c.scm:var? fun)
+           (var-local-fun fun))
+      `(,fun ,@(var-local-fun fun) ,@(c4args args)) ;; ローカル関数呼び出し
+      `(,fun ,@(c4args args))))
+
+(define (c4args args)
+  (if (null? args)
+      '()
+      (cons (c4expr (car args))
+            (c4args (cdr args)))))
+
